f056b8ec3db327f9ce3248c1070a267a31dbc59b
Francesco Paissan <me@francescopaissan.it>
2021-12-13 17:01:35 +0100

    Minor fixes to label sampling

--- modules/data_module.py
+++ modules/data_module.py
@@ -6,13 +6,18 @@ Authors
 from typing import Tuple, List
 from numpy import genfromtxt, ndarray, array
 import numpy as np
+from numpy.lib.npyio import load
 from scipy.stats import zscore
-from random import shuffle
+from random import shuffle, seed
 from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 from pytorch_lightning import LightningDataModule
 from pathlib import Path
 import torch
+from torch.utils.data.dataset import Subset
+
+seed(2408)
+np.random.seed(1305)
 
 
 class EEGDenoiseDataset(Dataset):
@@ -41,7 +46,10 @@ class EEGDenoiseDataset(Dataset):
         rep = np.ceil(len(clean_EEG) / len(noise_EEG))
         noise_EEG = np.repeat(noise_EEG, rep, axis=0)[: len(clean_EEG), :]
 
-        return (zscore(clean_EEG + noise_EEG, axis=1), noise[1])
+        return (
+            zscore(clean_EEG + noise_EEG, axis=1),
+            array([noise[1][0]] * len(noise_EEG)),
+        )
 
     def __init__(
         self, file_path: Path = Path("data/"), split: str = "", transforms: List = []
@@ -60,10 +68,6 @@ class EEGDenoiseDataset(Dataset):
 
         self.X = np.concatenate((data_eog[0], data_emg[0], data_clean[0]), axis=0)
         self.y = np.concatenate((data_eog[1], data_emg[1], data_clean[1]), axis=0)
-
-        indices = shuffle(np.arange(len(self.X)))
-        self.X = self.X[indices]
-        self.y = self.y[indices]
 
     def __getitem__(self, index: int) -> Tuple[Tensor, int]:
         return self.X[index], self.y[index]
@@ -92,11 +96,15 @@ class EEGDenoiseDM(LightningDataModule):
 
         data = EEGDenoiseDataset(file_path)
         train, self.test = torch.utils.data.random_split(
-            data, [int(np.ceil(len(data) * 0.3)), int(np.floor(len(data) * 0.7))]
+            data,
+            [int(np.floor(len(data) * 0.75)), int(np.ceil(len(data) * 0.25))],
+            generator=torch.Generator().manual_seed(1305),
         )
 
         self.train, self.val = torch.utils.data.random_split(
-            train, [int(np.ceil(len(train) * 0.3)), int(np.floor(len(train) * 0.7))]
+            train,
+            [int(np.floor(len(train) * 0.7)), int(np.ceil(len(train) * 0.3))],
+            generator=torch.Generator().manual_seed(2408),
         )
 
     def setup(self, stage=None):
@@ -110,3 +118,25 @@ class EEGDenoiseDM(LightningDataModule):
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(self.test, **self.dl_params)
+
+
+# if __name__ == "__main__":
+# a = EEGDenoiseDM()
+
+# X, y = [], []
+# for s in a.train:
+#     X.append(s[0])
+#     y.append(s[1])
+
+# X = array(X)
+# y = array(y)
+# print(y.shape)
+
+# from scipy.io import savemat
+
+# savemat("train_set.mat", {"X": X, "y": y})
+
+# from scipy.io import loadmat
+
+# data = loadmat("val_set.mat")
+# print(data["X"].shape, data["y"].T.shape)
